c78f480ebd2a4920143ba90cfbfbff34c6358e20
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-02-23 10:11:17 -0400

    Se implemento la funcionalidad de multiplicar paralelamente 2 numeros

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,15 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
+const third = document.querySelector("#tercer-numero");
+const fourth = document.querySelector("#cuarto-numero");
 const form = document.querySelector("#sumar-form");
+const form2 = document.querySelector("#multiplicar-form");
 const div = document.querySelector("#resultado-div");
+const div2 = document.querySelector("#resultado-div2");
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,5 +17,15 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p> Resultado = " + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const thirdNumber = Number.parseInt(third.value);
+  const fourthNumber = Number.parseInt(fourth.value);
+
+  div2.innerHTML = "<p> Resultado = " + multiplicar(thirdNumber, fourthNumber) + "</p>";
 });
